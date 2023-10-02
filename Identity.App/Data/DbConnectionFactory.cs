ca60b3e49a79dd08b05db70b5429023a489d09c0
weygerus <gabrileao38@gmail.com>
2023-10-02 20:16:15 -0300

    Criação da implementação da interface de criação de conexão SQL via injeção de dependencia

---
+++ Identity.App/Data/DbConnectionFactory.cs
@@ -0,0 +1,22 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Identity.App.Data
+{
+    public class DatabaseConnectionFactory : IDbConnectionInterface
+    {
+        private readonly string? _ConnectionString;
+
+        public DatabaseConnectionFactory(IConfiguration configuration)
+        {
+            _ConnectionString = configuration.GetConnectionString("DatabaseConnectionString");
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            IDbConnection connection = new SqlConnection(_ConnectionString);
+            connection.Open();
+            return connection;
+        }
+    }
+}
